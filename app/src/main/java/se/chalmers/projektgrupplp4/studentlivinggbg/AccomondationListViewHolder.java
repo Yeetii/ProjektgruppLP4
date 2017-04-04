1ca0dfc30697cc10e9b9f652df8099bb1a20083d
Flex <flexibleterror@hotmail.com>
2017-04-04 18:01:14 +0200

    Commiting AccomondationList stuff

---
+++ app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/AccomondationListViewHolder.java
@@ -0,0 +1,15 @@
+package se.chalmers.projektgrupplp4.studentlivinggbg;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+//This is a very small helper class for creating a list of Accomondation-items
+class AccomondationListViewHolder {
+    TextView txtAddress;
+    TextView txtHouseType;
+    TextView txtArea;
+    TextView txtPrice;
+    TextView txtSearchers;
+    ImageView favourite;
+    ImageView image;
+}
