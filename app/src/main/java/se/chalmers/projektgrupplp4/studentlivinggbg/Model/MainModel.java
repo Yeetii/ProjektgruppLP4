4d7b9c63e36aeb7a7504447d07b761936f2eb050
Jonathan <jonathan.gildevall@gmail.com>
2017-04-07 11:14:26 +0200

    Started to build MainModel, made i a singleton

--- app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/Model/MainModel.java
+++ app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/Model/MainModel.java
@@ -1,4 +1,32 @@
 package se.chalmers.projektgrupplp4.studentlivinggbg.Model;
 
+import java.util.List;
+
 public class MainModel {
+
+    private static MainModel mainModel;
+
+    private SearchHandler searchHandler;
+
+    private Settings settings;
+
+    private List accommodations;
+
+    public static MainModel getReference () {
+        if (mainModel == null) {
+            mainModel = new MainModel();
+        }
+        return mainModel;
+    }
+
+    private MainModel () {
+        searchHandler = new SearchHandler();
+        settings = new Settings();
+        updateAccommodations();
+
+    }
+
+    public void updateAccommodations () {
+
+    }
 }
