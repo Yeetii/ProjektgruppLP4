3b0867a03766b6415507bee47fcc8783605bd025
Peter Gärdenäs <gardenas@student.chalmers.se>
2017-04-27 14:39:06 +0200

    Some improvements to ChalmersAdapter

--- app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/ChalmersAdapter.java
+++ app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/ChalmersAdapter.java
@@ -1,5 +1,7 @@
 package se.chalmers.projektgrupplp4.studentlivinggbg;
 
+import android.widget.Switch;
+
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
@@ -11,6 +13,8 @@ import java.util.List;
 
 import se.chalmers.projektgrupplp4.studentlivinggbg.Controller.MainController;
 import se.chalmers.projektgrupplp4.studentlivinggbg.Model.Accommodation;
+import se.chalmers.projektgrupplp4.studentlivinggbg.Model.AccommodationHost;
+import se.chalmers.projektgrupplp4.studentlivinggbg.Model.AccommodationHouseType;
 
 /**
  * Created by PG on 20/04/2017.
@@ -59,35 +63,96 @@ public class ChalmersAdapter implements AccommodationAdapter {
         public List<Accommodation> getAccommodations() {
             List<Accommodation> accommodations = new ArrayList<>();
             String infoString = objectSummaryImages;
-            while (infoString.contains("hej")) {
+            while (infoString.contains("<div class=\"Box ObjektListItem \">")) {
                 accommodations.add(parseAccommodation(infoString));
+                infoString = infoString.substring(infoString.indexOf("<div class=\"Box ObjektListItem \">") + 1);
+                infoString = infoString.substring(infoString.indexOf("<div class=\"Box ObjektListItem \">"));
             }
 
             return accommodations;
         }
 
         private Accommodation parseAccommodation(String infoString) {
+            String objectNumber = getAttribute(infoString, "object number");
             String street = getAttribute(infoString, "street");
-            String houseType = getAttribute(infoString, "house type");
-            return null;
+            AccommodationHouseType houseType = parseHouseType(getAttribute(infoString, "house type"));
+            int price = parsePrice(getAttribute(infoString, "price"));
+            double area = parseArea(getAttribute(infoString, "area"));
+            String description = getAttribute(infoString, "description");
+            String thumbnail = getAttribute(infoString, "thumbnail");
+
+            System.out.println(objectNumber);
+            System.out.println(street);
+            System.out.println(houseType);
+            System.out.println(price);
+            System.out.println(area);
+            System.out.println(description);
+            System.out.println(thumbnail);
+
+            return new Accommodation(objectNumber, street, houseType, price, area, 0, description, thumbnail, AccommodationHost.CHALMERS);
           //  return new Accommodation()
         }
 
+        private double parseArea (String areaString) {
+            return Double.parseDouble(areaString);
+        }
+
+        private int parsePrice(String priceString) {
+            priceString = priceString.replace(" ", "");
+            return Integer.parseInt(priceString);
+        }
+
+        private AccommodationHouseType parseHouseType (String houseTypeString) {
+
+            switch (houseTypeString) {
+                case "1 rum och kök":
+                    return AccommodationHouseType.ONE_ROOM;
+                case "1 rum och trinett":
+                    return AccommodationHouseType.KITCHENETTE;
+            }
+            System.out.println("Should add to switch!: " + houseTypeString);
+            return null;
+        }
+
         public String getAttribute(String infoString, String type) {
+            String firstStartTag = "";
+            String secondStartTag = ">";
+            String endTag = "<";
+
             switch (type) {
+                case "object number":
+                    firstStartTag = "refid=";
+                    secondStartTag = "";
+                    endTag = "\">";
+                    break;
                 case "street":
-                    infoString = infoString.substring(infoString.indexOf("<h3 class=\"ObjektAdress\">"));
-                    infoString.substring(infoString.indexOf(">") + 1, infoString.length() - 9);
+                    firstStartTag = "class=\"ObjektAdress\"";
+                    secondStartTag = "d\">";
                     break;
                 case "house type":
-                    infoString = infoString.substring(infoString.indexOf("<h3 class=\"ObjectTyp\">"));
-                    infoString.substring(infoString.indexOf(">") + 1, infoString.length() - 9);
+                    firstStartTag = "class=\"ObjektTyp\">";
+                    secondStartTag = "d\">";
                     break;
                 case "description":
-
+                    firstStartTag = "class=\"ObjektFritext\">";
+                    break;
+                case "rent":
+                    firstStartTag = "dd class=\"ObjektHyra\"";
+                    endTag = " kr/mån";
+                    break;
+                case "area":
+                    firstStartTag = "dd class=\"ObjektYta\"";
+                    endTag = "m&sup2";
+                    break;
+                case "thumbnail":
+                    firstStartTag = "data-src=";
+                    secondStartTag = "https";
+                    endTag = " ";
+                    break;
             }
 
-            return infoString;
+            infoString = infoString.substring(infoString.indexOf(firstStartTag));
+            return infoString.substring(infoString.indexOf(secondStartTag), infoString.indexOf(endTag));
         };
 
         public String getObjectSummaryApartments () {
