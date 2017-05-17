8af9f8e354b9cad390cc9a24c6eb5fec22026571
Jonathan <jonathan.gildevall@gmail.com>
2017-05-17 15:07:22 +0200

    SettingActivity refactored

--- app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/activity/SettingsActivity.java
+++ app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/activity/SettingsActivity.java
@@ -1,47 +1,12 @@
 package se.chalmers.projektgrupplp4.studentlivinggbg.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-
 import se.chalmers.projektgrupplp4.studentlivinggbg.BottomNavigationListener;
 import se.chalmers.projektgrupplp4.studentlivinggbg.R;
 
-import static android.content.Intent.FLAG_ACTIVITY_NO_ANIMATION;
-
 public class SettingsActivity extends AppCompatActivity {
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_search:
-                    Intent search = new Intent(SettingsActivity.this, MainSearchActivity.class);
-                    search.addFlags(FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(search);
-                    return true;
-                case R.id.navigation_favorites:
-                    Intent favorites = new Intent(SettingsActivity.this, FavoritesActivity.class);
-                    favorites.addFlags(FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(favorites);
-                    return true;
-                case R.id.navigation_notifications:
-                    Intent searchWatcher = new Intent(SettingsActivity.this, SearchWatcherActivity.class);
-                    searchWatcher.addFlags(FLAG_ACTIVITY_NO_ANIMATION);
-                    startActivity(searchWatcher);
-                    return true;
-                case R.id.navigation_settings:
-                    return true;
-            }
-            return false;
-        }
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
