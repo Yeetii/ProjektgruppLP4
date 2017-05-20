a66c3989e856f70116c33f3824a47ba9fbba5798
Flex <flexibleterror@hotmail.com>
2017-05-20 11:05:10 +0200

    Removed un-used code in AccommodationAdapter

--- app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/model/accommodation/AccommodationAdapter.java
+++ app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/model/accommodation/AccommodationAdapter.java
@@ -1,40 +1,11 @@
 package se.chalmers.projektgrupplp4.studentlivinggbg.model.accommodation;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+
 import java.util.List;
 
-/**
- * Created by PG on 20/04/2017.
- */
+
 
 public abstract class AccommodationAdapter {
-    public void updateAccommodations() {
-        List<Accommodation> adapterAccommodations = getAccommodations();
-        List<Accommodation> accommodations = Accommodation.getAccommodations();
-
-        for (int i = 0; i < adapterAccommodations.size(); i++) {
-            boolean alreadyExists = false;
-
-            for (int y = 0; y < accommodations.size(); y++) {
-                if (isSameAccommodation(adapterAccommodations.get(i), accommodations.get(y))) {
-                    alreadyExists = true;
-                    accommodations.get(y).update(adapterAccommodations.get(i));
-                    break;
-                }
-            }
-
-            if (!alreadyExists) {
-                accommodations.add(adapterAccommodations.get(i));
-            }
-        }
-    }
-
-    private boolean isSameAccommodation (Accommodation accommodationOne, Accommodation accommodationTwo) {
-        return accommodationOne.getObjectNumber().equals(accommodationTwo.getObjectNumber());
-    }
 
     public abstract List<Accommodation> getAccommodations();
 }
