ccebb7cad14c18c90e890a576c930173fe1083d4
Peter Gärdenäs <gardenas@student.chalmers.se>
2017-05-22 01:19:05 +0200

    File was untracked

---
+++ app/src/main/java/se/chalmers/projektgrupplp4/studentlivinggbg/view/SearchWatcherAdapter.java
@@ -0,0 +1,91 @@
+package se.chalmers.projektgrupplp4.studentlivinggbg.view;
+
+import android.app.Activity;
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.view.View;
+
+import java.util.List;
+
+import se.chalmers.projektgrupplp4.studentlivinggbg.R;
+import se.chalmers.projektgrupplp4.studentlivinggbg.controller.searchwatcher.SearchWatcherItemController;
+import se.chalmers.projektgrupplp4.studentlivinggbg.view.searchwatcher.SearchWatcherItemView;
+import se.chalmers.projektgrupplp4.studentlivinggbg.model.searchwatcher.SearchWatcherItem;
+
+
+/**
+ * Created by PG on 03/04/2017.
+ */
+
+public class SearchWatcherAdapter extends ArrayAdapter<SearchWatcherItem> {
+
+    private Activity activity;
+    private LayoutInflater inflater;
+    private Class<? extends Activity> targetClass;
+
+
+    public SearchWatcherAdapter(Context context, List<SearchWatcherItem> data, Activity activity, Class<? extends Activity> targetClass) {
+        super(context, R.layout.search_watcher_row_item, data);
+        this.targetClass = targetClass;
+        inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.activity = activity;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        SearchWatcherItem dataModel = getItem(position);
+        SearchWatcherItemView viewHolder;
+        System.out.println("Making list item " + position + " with title" + dataModel.getTitle());
+
+        if (convertView == null) {
+            if (dataModel.isExpanded()) {
+                convertView = inflater.inflate(R.layout.search_watcher_row_item_expanded, null);
+            } else {
+                convertView = inflater.inflate(R.layout.search_watcher_row_item, null);
+            }
+            viewHolder = new SearchWatcherItemView(dataModel, convertView);
+            new SearchWatcherItemController(dataModel, convertView, activity, targetClass);
+
+
+        }else{
+            viewHolder = (SearchWatcherItemView) convertView.getTag();
+        }
+
+        viewHolder.updateView(dataModel);
+        return convertView;
+    }
+
+    //When tapping on a SearchWatcherItem
+    /*
+    This should be done in the controller imo.
+    @Override
+    public void onClick(View v) {
+        int position=(Integer) v.getTag();
+        SearchWatcherItem tappedSearch = getItem(position);
+        //TODO Access the controller?
+
+        if (tappedSearch.isExpanded()) {
+            tappedSearch.resetExpanded();
+        } else {
+            tappedSearch.setExpanded();
+        }
+
+        switch (v.getId())
+        {
+            case R.id.searchWithSearchWatcherButton:
+                //TODO not used atm
+//                AdvancedSearchActivityController.advancedSearchButtonPressed(v);
+                break;
+        }
+    }
+    */
+
+    @Override
+    public long getItemId(int position){
+            return super.getItemId(position);
+    }
+}
